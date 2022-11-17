2607bd2099ba8b76042028002581aa92f164da05
taehoon7289 <taehoon7289@gmail.com>
2022-11-17 17:02:43 +0900

    feat: error.html 수정 & app-ads.txt 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import LayoutHeader from './layout/header'
 import LayoutMain from './layout/main'
-import LayoutFooter from './layout/footer'
+// import LayoutFooter from './layout/footer'
 
 const App = () => {
   return (
     <>
       <LayoutHeader />
       <LayoutMain />
-      <LayoutFooter />
     </>
   )
 }
