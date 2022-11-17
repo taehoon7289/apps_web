00b838d810d4b603883c76716c3abe848c546f4f
taehoon7289 <taehoon7289@gmail.com>
2022-11-17 18:29:37 +0900

    feat: 배너_기능 추가

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,13 +1,19 @@
 import BannerServiceIntro from 'assets/img/banner_service_intro.png'
+import BannerTech from 'assets/img/banner_tech.png'
 
 const LayoutMain = () => {
   return (
     <>
       <div className={'main'}>
         <div id={'service'}>
-          <img src={BannerServiceIntro} alt={'서비스소개'} />
+          <img src={BannerServiceIntro} alt={'서비스'} />
         </div>
-        {/* <div id={'tech'}>기능</div> */}
+        <div className={'title-bar'}>
+          <h1 className={'title-bar-text'}>기능</h1>
+        </div>
+        <div id={'tech'}>
+          <img src={BannerTech} alt={'기능'} />
+        </div>
         {/* <div id={'question'}>문의하기</div> */}
       </div>
     </>
